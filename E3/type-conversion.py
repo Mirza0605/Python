aa3ed7f0a63d52730ae3ec34d2aa243ed02df2ed
Melik Mirza Çelik <mecelik1905@gmail.com>
2026-07-04 10:20:38 +0300

    tür dönüşümleri

---
+++ E3/type-conversion.py
@@ -0,0 +1,59 @@
+"""
+x = input('1.Sayı: ')
+y = input('2.Sayı: ')
+
+toplam = x + y
+print(toplam) => #inputtan gelen deper string olduğu için çıktı 1020 olur. 
+"""
+"""
+x = input('1.Sayı: ')
+y = input('2.Sayı: ')
+
+print(type(x))
+print(type(y))
+ 
+toplam= int(x) + int(y)
+print(toplam) => #burada tür dönüşümü yaptığımız için çıktıyı toplama göre verir.
+"""
+
+x = 5               #int
+y = 2.5             #float
+name = 'Mirza'      #string
+isOnline = True     #bool
+
+# print(type(x))
+# print(type(y))
+# print(type(name))
+# print(type(isOnline))
+
+# Typr Conversion
+
+# int to float
+
+# x = float(x)
+# print(x)
+# print(type(x)) => 5.0 çıktısını verir.
+
+# # float to int
+
+# y = int(y)
+# print(y)
+# print(type(y)) => 2 çıktısını verir.
+
+# result = x + y
+# print(result)
+# print(type(result)) => 7.5 verir.
+
+# result = str(x) + str(y)
+# print(result)
+# print(type(result)) => 52.5 verir.
+
+# bool to str
+
+# isOnline = str(isOnline)
+# print(isOnline)
+# print(type(isOnline)) => String olarak True değerini verir.
+
+# isOnline = int(isOnline)
+# print(isOnline)
+# print(type(isOnline)) => 1 sonucunu verir.
