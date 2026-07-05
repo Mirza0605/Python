e057a194d479fc63ccc3d5efbaf1f3d0cb60333b
Melik Mirza Çelik <mecelik1905@gmail.com>
2026-07-05 10:47:23 +0300

    liste

---
+++ E3/lists.py
@@ -0,0 +1,24 @@
+# message = 'Hello There. My name is Mirza Çelik'.split()
+# print(message[0])
+
+# my_lists = [1,2,3]
+# my_lists = ['bir',2,True,5.6]
+# print(my_lists)
+
+list1 = ['one','two','there']
+list2 = ['four','five','six']
+
+numbers = list1 + list2
+print(numbers)
+print(len(numbers))
+
+userA = ['Mirza',20]
+userB = ['Furkan',24]
+
+users = [userA , userB]
+
+print(userA)
+print(userB)
+print(users)
+
+print(users[1][0]) => usersın 1. indexindeki ifadenin 0. indexinin alır.
